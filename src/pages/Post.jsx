917fa7c7752b6ece060c66aea10b46a9a09ef5e6
tanvir-hossen49 <thossen49@gmail.com>
2023-10-13 16:43:55 +0600

    added share link

--- src/pages/Post.jsx
+++ src/pages/Post.jsx
@@ -72,11 +72,31 @@ const Post = () => {
                     )}
                 </div>
                 <div className="my-3 text-center text-xl font-medium">
-                 <div>
-                 Author: <span className="capitalize">{post.author}</span>  | Published: { new Date(post.$createdAt).toLocaleDateString('en-US', {
-                        year: 'numeric', month: 'short', day: 'numeric'
-                    })}
-                 </div>
+                    <div>
+                    Author: <span className="capitalize">{post.author}</span>  | Published: { new Date(post.$createdAt).toLocaleDateString('en-US', {
+                            year: 'numeric', month: 'short', day: 'numeric'
+                        })}
+                    </div> 
+
+                    <div className="flex mt-3 items-center justify-center">
+                        <p className="mr-3">Share: </p>
+                        <div className="flex items-center gap-3">
+                            <div className="cursor-pointer border-2 rounded-full p-3 duration-300 hover:bg-black hover:text-white">
+                                <a href={`https://www.facebook.com/sharer/sharer.php?u=${encodeURIComponent(`
+                                https://blog-application-rho-murex.vercel.app/post/${slug}
+                                `)}`} target="blank">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-facebook"><path d="M18 2h-3a5 5 0 0 0-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 0 1 1-1h3z"/></svg>
+                                </a>
+                            </div>
+                            <div className="cursor-pointer border-2 rounded-full p-3 duration-300 hover:bg-black hover:text-white">
+                                <a href={ `https://www.linkedin.com/sharing/share-offsite/?url=${encodeURIComponent(`
+                                https://blog-application-rho-murex.vercel.app/post/${slug}
+                                `)}&title=${encodeURIComponent(post.title)}&summary=${encodeURIComponent(parse(post.content))}`} target="blank">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-linkedin"><path d="M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6z"/><rect width="4" height="12" x="2" y="9"/><circle cx="4" cy="4" r="2"/></svg>
+                                </a>
+                            </div>
+                    </div>
+                    </div>
                 </div>
                 <div className="w-full mb-6">
                     <h1 className="text-2xl font-bold">{post.title}</h1>
